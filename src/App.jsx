27562a5cf80fe725dd7ba612e03bc024ba7c7168
tarek-10 <tarek.mohamed105900@gmail.com>
2022-01-08 04:13:22 +0200

    some updating in filter component

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,40 @@ import Products from "./components/Products/Products";
 import data from "./data.json";
 export default function App() {
   const [products, setProducts] = useState(data);
+  const [sort, setSort] = useState("");
+  const [size, setSize] = useState("");
 
+  const handlerFitlerBySize = (e) => {
+    setSize(e.target.value);
+
+    if (e.target.value == "ALL") {
+      setProducts(data);
+    } else {
+      let productsClone = [...products];
+
+      let newProducts = productsClone.filter(
+        (p) => p.sizes.indexOf(e.target.value) != -1
+      );
+
+      setProducts(newProducts);
+    }
+  };
+
+  const handlerFilterByOrder = (e) => {
+    let order = e.target.value;
+    setSort(order);
+    let productClone = [...products];
+    let newProducts = productClone.sort(function (a, b) {
+      if (order == "Lowest") {
+        return a.price - b.price;
+      } else if (order == "Hightest") {
+        return b.price - a.price;
+      } else {
+        return a.id < b.id ? 1 : -1;
+      }
+    });
+    setProducts(newProducts);
+  };
   return (
     <>
       <div className="layout">
@@ -14,7 +47,12 @@ export default function App() {
         <main>
           <div className="wrapper">
             <Products products={products} />
-            <Filter />
+            <Filter
+              size={size}
+              sort={sort}
+              handlerFitlerBySize={handlerFitlerBySize}
+              handlerFilterByOrder={handlerFilterByOrder}
+            />
           </div>
         </main>
         <Footer />
